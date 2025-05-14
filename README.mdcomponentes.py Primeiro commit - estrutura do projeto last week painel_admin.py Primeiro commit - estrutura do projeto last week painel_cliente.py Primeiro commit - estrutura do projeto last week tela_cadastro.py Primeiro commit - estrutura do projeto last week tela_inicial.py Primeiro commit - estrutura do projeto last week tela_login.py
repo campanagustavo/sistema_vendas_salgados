25951d272664e3f42f5e710ccafa5b8644be51a1
marcusvcmaia <148342981+marcusvcmaia@users.noreply.github.com>
2025-05-14 18:20:14 -0300

    Create README.mdcomponentes.py Primeiro commit - estrutura do projeto last week painel_admin.py Primeiro commit - estrutura do projeto last week painel_cliente.py Primeiro commit - estrutura do projeto last week tela_cadastro.py Primeiro commit - estrutura do projeto last week tela_inicial.py Primeiro commit - estrutura do projeto last week tela_login.py
    notas de aula 13/05/25

---
+++ README.mdcomponentes.py Primeiro commit - estrutura do projeto last week painel_admin.py Primeiro commit - estrutura do projeto last week painel_cliente.py Primeiro commit - estrutura do projeto last week tela_cadastro.py Primeiro commit - estrutura do projeto last week tela_inicial.py Primeiro commit - estrutura do projeto last week tela_login.py
@@ -0,0 +1,25 @@
+classes para o Diagrama UML
+italico usuario.py
+cliente.py
+admin.py
+
+italico alimento.py
+bolo.py
+salgado.py
+
+pedido.py (usa o metodo mas nao herda)
+
+
+
+base_de_dados.py
+
+alimento.py
+
+bolo.py
++preco
++sabor
++tamanho
++descricao
+
+get.preco
+get.sabor
